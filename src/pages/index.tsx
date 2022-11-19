54ed4ef9d0796d1fad8d79900a27999bc6796464
jan-czerwinski <jan.ignacy.czerwinski@gmail.com>
2022-11-19 13:25:32 +0100

    fix package json

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,17 @@
+import Dropzone from 'react-dropzone';
+
 const HomePage = () => {
   return (
-    <div className='grid h-screen w-screen place-content-center'>hello</div>
+    <div className='grid h-screen w-screen place-content-center'>
+      <Dropzone onDrop={(acceptedFiles) => console.log(acceptedFiles)}>
+        {({ getRootProps, getInputProps }) => (
+          <div className='' {...getRootProps()}>
+            <input {...getInputProps()} />
+            <p>Drag 'n' drop some files here, or click to select files</p>
+          </div>
+        )}
+      </Dropzone>
+    </div>
   );
 };
 
